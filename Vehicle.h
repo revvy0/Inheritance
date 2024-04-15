ed8c9f691af63d44be0ebb6ee54944a850f013b9
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-15 16:21:19 -0400

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,22 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
+#include <string>
+using namespace std;
+
+class Vehicle {
+protected: 
+  string manufacturer;
+  int yearBuilt;
+
+public:
+  Vehicle(const string manufacturer, int yearBuilt);
+  string getManufacturer() const;
+  void setManufacturer(const string manufacturer);
+  int getYearBuilt(int yearBuilt);
+  void displayInfo() const;
+}
+#endif // VEHICLE_H
+
+// this file has variables, functions, and the Vehicle class.
+// this vehicle file will be used in main
