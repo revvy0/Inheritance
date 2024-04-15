3ea75d89425c37641213a8048f8aa3ebe99b661b
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-15 16:25:17 -0400

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,1 +1,19 @@
+#ifndef CAR_H
+#define CAR_H
 
+#include "Vehicle.h"
+using namespace std;
+
+class Car : public Vehicle {
+private:
+  int doorNums;
+
+public:
+  Car(const string manufacturer, int yearBuilt, int doorNums);
+  int getDoorNums() const;
+  void setDoorNum(int doorNums);
+  void displayInfo() const override;
+}
+#endif // CAR_H
+// same as vehicle.h even though vehicle.h is present in this file.
+// assumed use will be in car.cpp and main?
