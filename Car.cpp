9efc7d5be6eeacb37fd9f2d81c922a61eea8c72e
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-16 13:11:23 -0400

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,1 +1,18 @@
+#include "Car.h"
+#include <iostream>
 
+using namespace std; 
+
+Car::Car(const string manufacturer, int yearBuilt, int doorNums) : Vehicle(manufacturer, yearBuilt), doorNums(doorNums) {}
+
+int Car::getdoorNum() const {
+  return doorNums;
+}
+void Car::setDoorNums(int doorNums) {
+  this->doorNums = doorNums;
+}
+void Car::displayInfo() const {
+  Vehicle::displayInfo();
+  cout << "Number of Doors: " << doorNums << endl;
+}
+// come back to add and change later
