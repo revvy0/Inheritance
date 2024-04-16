8f677ba3f184918d44f2c2d9af4d500f9d645207
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-16 14:40:36 -0400

    Update Truck.cpp

--- Truck.cpp
+++ Truck.cpp
@@ -1,1 +1,17 @@
+#include "Truck.h"
+#include <iostream>
 
+using namespace std;
+
+Truck::Truck(const string manufacturer, int yearBuilt, int towingCapacity) : Vehicle(manufacturer, yearBuilt), towingCapacity(towingCapacity) {}
+
+int Truck:getTowingCapacity() const
+  return towingCapacity;
+}
+void Truck::setTowingCapacity(int towingCapacity) {
+  this->towingCapacity = towingCapacity;
+}
+void Truck::displayInfo() const {
+  Vehicle::displayInfo();
+  cout << "Towing Capacity: " << towingCapacity << endl;
+}
