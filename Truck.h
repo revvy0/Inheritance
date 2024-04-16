f2ff47f66f60c7ffbcd5fc2d6df0edc0ec6ee377
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-16 12:39:35 -0400

    Update Truck.h

--- Truck.h
+++ Truck.h
@@ -1,1 +1,17 @@
+#ifndef TRUCK_H
+#define TRUCK_H
 
+#include "Vehicle.h"
+using namespace std;
+
+class Truck : public Vehicle {
+private: 
+  int towingCapacity;
+
+public:
+  Truck(const string manufacturer, int yearBuilt, int towingCapacity);
+  int getTowingCapacity() const;
+  void setTowingCapacity(int towingCapacity);
+  void displayInfo() const override; 
+}
+#endif // TRUCK_H
