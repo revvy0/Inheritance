62bcb6d0328234f48999c2799c3ad7385ab38e39
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-16 14:47:40 -0400

    Update mainSource.cpp
    come back later

--- mainSource.cpp
+++ mainSource.cpp
@@ -1,1 +1,33 @@
+// Paul Shelley
+// CIS 1202.501
+// 4/16/24
+#include <iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
 
+using namespace std;
+
+int main() {
+  // This is for Vehicle
+  string manufacturer;
+  int yearBuilt;
+  cout << "Who is the manufacturer of (VEHICLE): ";
+  cin.ignore();
+  getline(cin, manufacturer);
+  cout << endl;
+  cout << "What year was this vehicle built: ";
+  cin >> yearBuilt;
+  Vehicle vehicle(manufacturer, yearBuilt);
+  cout << "Info entered: " << endl;
+  vehicle.displayInfo();
+  // This is for Car
+  int doorNums;
+  cout << "How many doors: ";
+  cin >> doorNums;
+  Car car(manufacturer, yearBuilt, doorNums);
+  cout << "Info for car entered: " << endl;
+  car.displayInfo();
+  // This is for Truck
+  
+}
