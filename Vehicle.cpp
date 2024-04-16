da37864d4e076978b9a77eca9da63d7d2163d9ff
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-16 13:06:33 -0400

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,1 +1,24 @@
+#include "Vehicle.h"
+#include <iostream>
+
+using namespace std;
+
+Vehicle::Vehicle(const string manufacturer, int yearBuilt) : manufacturer(manufacturer), yearbuilt(yearBuilt) {}
+
+string Vehicle::getManufacturer() const { // get the manufacturer
+  return manufacturer;
+}
+void Vehicle::setManufacturer(const string manufacturer) { // setting manufacturer
+  this->manufacturer = manufacturer; // navigating
+}
+int Vehicle::getYearBuilt() const { // get the year built
+  return yearBuilt;
+}
+void Vehicle::setYearBuild(int yearBuilt) { // set year built
+  this->yearBuilt = yearBuilt;
+}
+void Vehicle::displayInfo() const { // displaying the info gathered from previous functions
+  cout << "Manufacturer: " << manufacturer << endl;
+  cout << "Year Built: " << yearBuilt << endl;
+}
 
