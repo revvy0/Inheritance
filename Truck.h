5a3b5790cb1d205bafae813b5360670d7a90e72f
revvy0 <163015200+revvy0@users.noreply.github.com>
2024-04-17 14:27:02 -0400

    Update Truck.h
    some changes

--- Truck.h
+++ Truck.h
@@ -5,13 +5,13 @@
 using namespace std;
 
 class Truck : public Vehicle {
-private: 
-  int towingCapacity;
+private:
+	int towingCapacity;
 
 public:
-  Truck(const string manufacturer, int yearBuilt, int towingCapacity);
-  int getTowingCapacity() const;
-  void setTowingCapacity(int towingCapacity);
-  void displayInfo() const; 
+	Truck(const string manufacturer, int yearBuilt, int towingCapacity);
+	int getTowingCapacity() const;
+	void setTowingCapacity(int towingCapacity);
+	void displayInfo() const;
 };
 #endif // TRUCK_H
